f0876d24875895ae6e14f87c1d8207674b881e37
Nishan Wijesinghe <nishan@parigan.com>
2021-06-04 23:25:57 -0400

    Spot vs on-demand price introspection

--- price_introspection.sh
+++ price_introspection.sh
@@ -1,11 +1,18 @@
+#On-demand pricing: regional
+#Spot pricing: zonal
+
 location="US East (N. Virginia)"
-region="us-east-1"
+region=$(terraform output -raw region)
 instance_type="t3a.xlarge"
 
-#Spot market price
-aws ec2 describe-spot-price-history --start-time=$(date +%s) --product-descriptions="Linux/UNIX" --instance-types t3a.xlarge --region ${region}
+echo "Spot price on cheapest zone"
+aws ec2 describe-spot-price-history --start-time=$(date +%s) --product-descriptions="Linux/UNIX" --instance-types ${instance_type} --region ${region} \
+  | jq '.SpotPriceHistory|=sort_by(.SpotPrice)' \
+  | jq '.SpotPriceHistory[] | .AvailabilityZone + " $" + .SpotPrice' \
+  | python -c "import sys; print(input())"
 
-#On demand & reserved market price
+
+echo "On demand & reserved market price"
 aws pricing get-products --service-code AmazonEC2 \
 --filters "Type=TERM_MATCH,Field=instanceType,Value=${instance_type}" \
           "Type=TERM_MATCH,Field=location,Value=${location}" \
@@ -26,3 +33,13 @@ aws pricing get-products --service-code AmazonEC2 \
         .terms.OnDemand[].priceDimensions[].pricePerUnit.USD,
         .terms.OnDemand[].priceDimensions[].description] | @csv'
 
+#TODO filter out reserved market prices
+#|  python -c "import sys; input_lines = sys.stdin.readlines(); for item in input_lines: if ""On Demand Linux"" in item: print(item)"
+#| select( .terms.OnDemand[].priceDimensions[].description | contains("On Demand Linux"))
+
+# Cost savings from spot to on-demand
+
+spot_price=$(aws ec2 describe-spot-price-history --start-time=$(date +%s) --product-descriptions="Linux/UNIX" --instance-types ${instance_type} --region ${region} \
+  | jq '.SpotPriceHistory|=sort_by(.SpotPrice)' \
+  | jq '.SpotPriceHistory[] | .SpotPrice' \
+  | python -c "import sys; print(input())")
